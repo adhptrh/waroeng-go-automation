8214ee941f748338b49d85d21ea5f4e882c565b1
Azmy39 <adhikaputrahermanda39@gmail.com>
2021-05-15 02:50:51 +0700

    discord inner class

--- bot/bot.py
+++ bot/bot.py
@@ -8,7 +8,7 @@ class Bot:
         "channel_id":"channel_id"
     }
 
-    api_link = "https://discord.com/api/v9"
+    discord = ""
 
     def __init__(self,config_path):
         self.cfg_path = config_path
@@ -21,22 +21,29 @@ class Bot:
             c = open(self.cfg_path,"w")
             self.data = self.blankData()
             c.write(json.dumps(self.data))
+        
+        self.reauthDiscord()
 
     def getAuthorization(self):
         return self.data[self.cfg_names["auth"]]
 
     def setAuthorization(self,new_auth):
         self.data[self.cfg_names["auth"]] = new_auth
+        self.reauthDiscord()
 
     def getChannelId(self):
         return self.data[self.cfg_names["channel_id"]]
 
     def setChannelId(self, new_channel_id):
         self.data[self.cfg_names["channel_id"]] = new_channel_id
+        self.reauthDiscord()
 
     def save(self):
         s = open(self.cfg_path,"w")
         s.write(json.dumps(self.data))
+
+    def reauthDiscord(self):
+        self.discord = Bot.Discord(self.getAuthorization(),self.getChannelId())
 
     def blankData(self):
         return {
@@ -46,21 +53,29 @@ class Bot:
 
     def reset(self):
         self.data = self.blankData()
-
         self.save()
 
-    def generateHeader(self):
-        return {
-            "authorization":self.data[self.getAuthorization()]
-        }
+    class Discord:
+        api_link = "https://discord.com/api/v9"
+        authorization = ""
+        channel_id = ""
 
-    def getLastChannelMessage(self):
-        r = requests.get(f"{self.api_link}/channels/{self.getChannelId()}/messages?limit=1",headers=self.generateHeader())
-        return r.json()
+        def generateHeader(self):
+            return {
+                "authorization":self.authorization
+            }
+        
+        def __init__(self,authorization,channel_id):
+            self.authorization = authorization
+            self.channel_id = channel_id
 
-    def sendMessage(self,message):
-        pdata = {
-            "content":message
-        }
-        r = requests.post(f"{self.api_link}/channels/{self.getChannelId()}/messages",headers=self.generateHeader(),json=pdata)
-        return r.json()
+        def getLastChannelMessage(self):
+            r = requests.get(f"{self.api_link}/channels/{self.channel_id}/messages?limit=1",headers=self.generateHeader())
+            return r.json()
+
+        def sendMessage(self,message):
+            pdata = {
+                "content":message
+            }
+            r = requests.post(f"{self.api_link}/channels/{self.channel_id}/messages",headers=self.generateHeader(),json=pdata)
+            return r.json()
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import requests, time, json, re
 from bot.bot import Bot
-config = Bot("config.json")
+bot = Bot("config.json")
 
 print("""
 Waroeng-Go Automation Bot
 [=---------------------------=]
 """)
 
-if not config.getAuthorization():
-    config.setAuthorization(input("Put your discord authorization here : "))
+if not bot.getAuthorization():
+    bot.setAuthorization(input("Put your discord authorization here : "))
 
-if not config.getChannelId():
-    config.setChannelId(input("Put your server channel id : "))
+if not bot.getChannelId():
+    bot.setChannelId(input("Put your server channel id : "))
 
-config.save()
+bot.save()
+print(bot.discord.sendMessage("test 2"))
